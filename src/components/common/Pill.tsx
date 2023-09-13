4affc33b18f7a28d48d01c073c7322f727e4a84e
Chinnapat.N <kharkkr@gmail.com>
2023-09-13 15:39:28 +0700

    Implement Pill

---
+++ src/components/common/Pill.tsx
@@ -0,0 +1,28 @@
+import { PILL_LABEL } from '@/constants/LABEL';
+import { Chip } from '@mui/material';
+
+interface PillProps {
+	variant: keyof typeof PILL_LABEL;
+}
+
+export default function Pill(props: PillProps) {
+	return (
+		<Chip
+			sx={{
+				height: '20px',
+				'& .MuiChip-label': {
+					p: 0,
+				},
+			}}
+			label={<p className="text-h5">{props.variant}</p>}
+			variant="outlined"
+			className={`px-4 py-0 border-2 ${
+				props.variant === 'INACTIVE'
+					? 'text-light_text_grey border-light_text_grey'
+					: props.variant === 'EMERGENCY'
+					? 'text-error_red border-error_red'
+					: 'text-active_green border-active_green'
+			} rounded-sm`}
+		/>
+	);
+}
