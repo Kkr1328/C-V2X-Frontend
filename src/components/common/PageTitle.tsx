f289a30aa27eec9b29d1bb1903edc7e15328b948
Chinnapat.N <kharkkr@gmail.com>
2023-09-13 15:50:09 +0700

    Implement page title

---
+++ src/components/common/PageTitle.tsx
@@ -0,0 +1,16 @@
+import { Stack } from '@mui/material';
+
+import IconMapper from '@/utils/IconMapper';
+
+interface PageTitleProp {
+	title: string;
+}
+
+export default function PageTitle(props: PageTitleProp) {
+	return (
+		<Stack direction="row" className="gap-16 items-center text-black">
+			<IconMapper icon={props.title} size="48px" />
+			<p className="text-h2 text-black">{props.title}</p>
+		</Stack>
+	);
+}
