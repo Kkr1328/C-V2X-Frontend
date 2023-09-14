3f11c226be95150b5c6b87344cb5b8697bb5d97e
Chinnapat.N <kharkkr@gmail.com>
2023-09-15 00:52:29 +0700

    Retype label file

---
+++ src/constants/LABEL.tsx
@@ -0,0 +1,50 @@
+export enum NAVBAR_LABEL {
+	DASHBOARD = 'Dashboard',
+	OVERVIEW = 'Overview',
+	CAMERA = 'Camera',
+	EMERGENCY = 'Emergency',
+	HEARTBEAT = 'Heartbeat',
+	ENTITY_MANAGEMENT = 'Entity Management',
+	CARS = 'Cars',
+	DRIVERS = 'Drivers',
+	CAMERAS = 'Cameras',
+	RSUS = 'RSUs',
+	EXPAND = 'Expand',
+	COLLAPSE = 'Collapse',
+}
+
+export enum INPUT_LABEL {
+	ID = 'ID',
+	FRONT_CAMERA = 'Front Camera',
+	BACK_CAMERA = 'Back Camera',
+	NAME = 'Name',
+	LICENSE_PLATE = 'License Plate',
+	MODEL = 'Model',
+	DRIVER = 'Driver',
+	FIRST_NAME = 'First Name',
+	LAST_NAME = 'Last Name',
+	USERNAME = 'Username',
+	PASSWORD = 'Password',
+	PHONE_NO = 'Phone NO.',
+	CAR = 'Car',
+	POSITION = 'Position',
+	RECOMENDED_SPEED = 'Recommended Speed (km/h)',
+}
+
+export enum BUTTON_LABEL {
+	CLEAR = 'Clear',
+	SEARCH = 'Search',
+	REGISTER = 'Register',
+	REFRESH = 'Refresh',
+	EDIT = 'Edit',
+	DELETE = 'Delete',
+	CANCLE = 'Cancle',
+	CONFIRM = 'Confirm',
+	ZOOM = 'Zoom',
+}
+
+export enum PILL_LABEL {
+	ACTIVE = 'ACTIVE',
+	INACTIVE = 'INACTIVE',
+	EMERGENCY = 'EMERGENCY',
+}
