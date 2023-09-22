6b99c1d2c0e73879bc9ed22df28dd36372a796fe
Chinnapat.N <kharkkr@gmail.com>
2023-09-22 13:45:34 +0700

    Add register modal to entity pages

--- src/app/entity_management/cars/page.tsx
+++ src/app/entity_management/cars/page.tsx
@@ -1,34 +1,54 @@
-import { BUTTON_LABEL, NAVBAR_LABEL } from '@/constants/LABEL';
+'use client';
+
+import { BUTTON_LABEL, MODAL_LABEL, NAVBAR_LABEL } from '@/constants/LABEL';
 import PageTitle from '@/components/common/PageTitle';
 import { Card, Divider, Stack } from '@mui/material';
 import Filter from '@/components/module/Filter';
 import ButtonCV2X from '@/components/common/ButtonCV2X';
-import { CarFilterTemplate } from '@/constants/TEMPLATE';
+import {
+	CarActionModalTemplate,
+	CarFilterTemplate,
+} from '@/constants/TEMPLATE';
+import ModalCV2X from '@/components/common/ModalCV2X';
+import ModalInputs from '@/components/module/ModalInputs';
+import { useState } from 'react';
 
 export default function Home() {
+	const [openRegisterModal, setOpenRegisterModal] = useState<boolean>(false);
+
 	return (
-		<Stack className="gap-16">
-			<PageTitle title={NAVBAR_LABEL.CARS} />
-			<Card className="w-full h-full rounded-lg px-32 py-24">
-				<Stack className="gap-16">
-					<Filter template={CarFilterTemplate} />
-					<Divider />
-					<Stack direction="row" className="gap-8">
-						<p className="text-dark_text_grey text-h5 self-center">{`Total(10)`}</p>
-						<div className="grow" />
-						<ButtonCV2X
-							icon={BUTTON_LABEL.REGISTER}
-							label={BUTTON_LABEL.REGISTER_CAR}
-							variant="contained"
-						/>
-						<ButtonCV2X
-							icon={BUTTON_LABEL.REFRESH}
-							label={BUTTON_LABEL.REFRESH}
-							variant="outlined"
-						/>
+		<>
+			<ModalCV2X
+				title={MODAL_LABEL.REGISTER_CAR}
+				variant="Register"
+				open={openRegisterModal}
+				handleOnClose={() => setOpenRegisterModal(false)}>
+				<ModalInputs template={CarActionModalTemplate} />
+			</ModalCV2X>
+			<Stack className="gap-16">
+				<PageTitle title={NAVBAR_LABEL.CARS} />
+				<Card className="w-full h-full rounded-lg px-32 py-24">
+					<Stack className="gap-16">
+						<Filter template={CarFilterTemplate} />
+						<Divider />
+						<Stack direction="row" className="gap-8">
+							<p className="text-dark_text_grey text-h5 self-center">{`Total(10)`}</p>
+							<div className="grow" />
+							<ButtonCV2X
+								icon={BUTTON_LABEL.REGISTER}
+								label={BUTTON_LABEL.REGISTER_CAR}
+								variant="contained"
+								onClick={() => setOpenRegisterModal(true)}
+							/>
+							<ButtonCV2X
+								icon={BUTTON_LABEL.REFRESH}
+								label={BUTTON_LABEL.REFRESH}
+								variant="outlined"
+							/>
+						</Stack>
 					</Stack>
-				</Stack>
-			</Card>
-		</Stack>
+				</Card>
+			</Stack>
+		</>
 	);
 }
