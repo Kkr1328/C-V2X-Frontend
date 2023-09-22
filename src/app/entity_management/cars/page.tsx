7bf1e2b9f200ce3c6d1090c14cd9d083f6489bd3
Chinnapat.N <kharkkr@gmail.com>
2023-09-22 10:00:27 +0700

    Add filter to entity pages

--- src/app/entity_management/cars/page.tsx
+++ src/app/entity_management/cars/page.tsx
@@ -1,10 +1,34 @@
-import { NAVBAR_LABEL } from '@/constants/LABEL';
+import { BUTTON_LABEL, NAVBAR_LABEL } from '@/constants/LABEL';
 import PageTitle from '@/components/common/PageTitle';
+import { Card, Divider, Stack } from '@mui/material';
+import Filter from '@/components/module/Filter';
+import ButtonCV2X from '@/components/common/ButtonCV2X';
+import { CarFilterTemplate } from '@/constants/TEMPLATE';
 
 export default function Home() {
 	return (
-		<>
+		<Stack className="gap-16">
 			<PageTitle title={NAVBAR_LABEL.CARS} />
-		</>
+			<Card className="w-full h-full rounded-lg px-32 py-24">
+				<Stack className="gap-16">
+					<Filter template={CarFilterTemplate} />
+					<Divider />
+					<Stack direction="row" className="gap-8">
+						<p className="text-dark_text_grey text-h5 self-center">{`Total(10)`}</p>
+						<div className="grow" />
+						<ButtonCV2X
+							icon={BUTTON_LABEL.REGISTER}
+							label={BUTTON_LABEL.REGISTER_CAR}
+							variant="contained"
+						/>
+						<ButtonCV2X
+							icon={BUTTON_LABEL.REFRESH}
+							label={BUTTON_LABEL.REFRESH}
+							variant="outlined"
+						/>
+					</Stack>
+				</Stack>
+			</Card>
+		</Stack>
 	);
 }
--- src/app/entity_management/drivers/page.tsx
+++ src/app/entity_management/drivers/page.tsx
@@ -1,10 +1,34 @@
+import ButtonCV2X from '@/components/common/ButtonCV2X';
 import PageTitle from '@/components/common/PageTitle';
-import { NAVBAR_LABEL } from '@/constants/LABEL';
+import Filter from '@/components/module/Filter';
+import { BUTTON_LABEL, NAVBAR_LABEL } from '@/constants/LABEL';
+import { DriverFilterTemplate } from '@/constants/TEMPLATE';
+import { Card, Divider, Stack } from '@mui/material';
 
 export default function Home() {
 	return (
-		<>
+		<Stack className="gap-16">
 			<PageTitle title={NAVBAR_LABEL.DRIVERS} />
-		</>
+			<Card className="w-full h-full rounded-lg px-32 py-24">
+				<Stack className="gap-16">
+					<Filter template={DriverFilterTemplate} />
+					<Divider />
+					<Stack direction="row" className="gap-8">
+						<p className="text-dark_text_grey text-h5 self-center">{`Total(10)`}</p>
+						<div className="grow" />
+						<ButtonCV2X
+							icon={BUTTON_LABEL.REGISTER}
+							label={BUTTON_LABEL.REGISTER_DRIVER}
+							variant="contained"
+						/>
+						<ButtonCV2X
+							icon={BUTTON_LABEL.REFRESH}
+							label={BUTTON_LABEL.REFRESH}
+							variant="outlined"
+						/>
+					</Stack>
+				</Stack>
+			</Card>
+		</Stack>
 	);
 }
