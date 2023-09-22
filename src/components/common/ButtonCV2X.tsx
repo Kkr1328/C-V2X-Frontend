4a8acef41b78ee849c488500ab2b2bc880e1e477
Chinnapat.N <kharkkr@gmail.com>
2023-09-22 10:03:07 +0700

    Minor fix

--- src/components/common/ButtonCV2X.tsx
+++ src/components/common/ButtonCV2X.tsx
@@ -13,33 +13,33 @@ interface ButtonCV2XProp {
 
 export default function ButtonCV2X(props: ButtonCV2XProp) {
 	const containedPrimaryStyle: string =
-		'normal-case h-44 text-white bg-primary_blue hover:bg-dark_primary_blue disabled:text-light_text_grey disabled:bg-dark_background_grey';
+		'text-white bg-primary_blue hover:bg-dark_primary_blue disabled:text-light_text_grey disabled:bg-dark_background_grey';
 	const containedSecondaryStyle: string =
-		'normal-case h-44 text-white bg-light_text_grey hover:bg-dark_text_grey disabled:text-light_text_grey disabled:bg-dark_background_grey';
+		'text-white bg-light_text_grey hover:bg-dark_text_grey disabled:text-light_text_grey disabled:bg-dark_background_grey';
 	const containedAcceptStyle: string =
-		'normal-case h-44 text-white bg-active_green hover:bg-dark_active_green disabled:text-light_text_grey disabled:bg-dark_background_grey';
+		'text-white bg-active_green hover:bg-dark_active_green disabled:text-light_text_grey disabled:bg-dark_background_grey';
 	const containedErrorStyle: string =
-		'normal-case h-44 text-white bg-error_red hover:bg-dark_error_red disabled:text-light_text_grey disabled:bg-dark_background_grey';
+		'text-white bg-error_red hover:bg-dark_error_red disabled:text-light_text_grey disabled:bg-dark_background_grey';
 
 	const textPrimaryStyle: string =
-		'normal-case h-44 text-primary_blue hover:bg-dark_background_blue disabled:text-light_text_grey';
+		'text-primary_blue hover:bg-dark_background_blue disabled:text-light_text_grey';
 	const textSecondaryStyle: string =
-		'normal-case h-44 text-light_text_grey hover:bg-dark_background_grey disabled:text-light_text_grey';
+		'text-light_text_grey hover:bg-dark_background_grey disabled:text-light_text_grey';
 	const textAcceptStyle: string =
-		'normal-case h-44 text-active_green hover:bg-dark_background_green disabled:text-light_text_grey';
+		'text-active_green hover:bg-dark_background_green disabled:text-light_text_grey';
 	const textErrorStyle: string =
-		'normal-case h-44 text-error_red hover:bg-dark_background_red disabled:text-light_text_grey';
+		'text-error_red hover:bg-dark_background_red disabled:text-light_text_grey';
 
 	const outlinedPrimaryStyle: string =
-		'normal-case h-44 text-primary_blue border-primary_blue border-2 bg-white hover:border-primary_blue hover:border-2 hover:bg-dark_background_blue disabled:border-light_text_grey disabled:border-2 disabled:text-light_text_grey disabled:bg-dark_background_grey';
+		'text-primary_blue border-primary_blue border-2 bg-white hover:border-primary_blue hover:border-2 hover:bg-dark_background_blue disabled:border-light_text_grey disabled:border-2 disabled:text-light_text_grey disabled:bg-dark_background_grey';
 	const outlinedSecondaryStyle: string =
-		'normal-case h-44 text-light_text_grey border-light_text_grey border-2 bg-white hover:border-light_text_grey hover:border-2 hover:bg-dark_background_grey disabled:border-light_text_grey disabled:border-2 disabled:text-light_text_grey disabled:bg-dark_background_grey';
+		'text-light_text_grey border-light_text_grey border-2 bg-white hover:border-light_text_grey hover:border-2 hover:bg-dark_background_grey disabled:border-light_text_grey disabled:border-2 disabled:text-light_text_grey disabled:bg-dark_background_grey';
 	const outlinedAcceptStyle: string =
-		'normal-case h-44 text-active_green border-active_green border-2 bg-white hover:border-active_green hover:border-2 hover:bg-dark_background_green disabled:border-light_text_grey disabled:border-2 disabled:text-light_text_grey disabled:bg-dark_background_grey';
+		'text-active_green border-active_green border-2 bg-white hover:border-active_green hover:border-2 hover:bg-dark_background_green disabled:border-light_text_grey disabled:border-2 disabled:text-light_text_grey disabled:bg-dark_background_grey';
 	const outlinedErrorStyle: string =
-		'normal-case h-44 text-error_red border-error_red border-2 bg-white hover:border-error_red hover:border-2 hover:bg-dark_background_red disabled:border-light_text_grey disabled:border-2 disabled:text-light_text_grey disabled:bg-dark_background_grey';
+		'text-error_red border-error_red border-2 bg-white hover:border-error_red hover:border-2 hover:bg-dark_background_red disabled:border-light_text_grey disabled:border-2 disabled:text-light_text_grey disabled:bg-dark_background_grey';
 
-	const buttonStyle: string = `p-12 gap-8 m-none rounded-md ${
+	const buttonStyle: string = `normal-case h-44 px-12 m-none gap-8 rounded-md ${
 		props.variant === 'text'
 			? props.color === 'secondary'
 				? textSecondaryStyle
