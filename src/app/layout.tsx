c9cc4152743fcb1c2418f0dddb9ce275fe9527cc
Chinnapat.N <kharkkr@gmail.com>
2023-09-23 18:12:36 +0700

    Update font importation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import Header from '@/components/common/Header';
-import Navbar from '@/components/module/Navbar';
+import { Istok_Web } from 'next/font/google';
+
 import { Box, Toolbar } from '@mui/material';
 
-const inter = Inter({ subsets: ['latin'] });
+import Header from '@/components/common/Header';
+import Navbar from '@/components/module/Navbar';
+
+const istok_web = Istok_Web({
+	weight: ['400', '700'],
+	subsets: ['latin'],
+	variable: '--font-istok-web',
+});
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -18,23 +24,19 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
-			<header>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Istok+Web&display=swap"
-				/>
-			</header>
-			<body className={inter.className}>
-				<Box className="w-screen h-screen flex bg-dark_background_grey">
-					<Header />
-					<Navbar />
-					<Box className="grow px-32 py-32">
-						<Toolbar />
-						{children}
+		<>
+			<html lang="en" className={`${istok_web.variable} font-sans`}>
+				<body suppressHydrationWarning={true}>
+					<Box className="w-screen h-screen flex bg-dark_background_grey">
+						<Header />
+						<Navbar />
+						<Box className="grow px-32 py-32">
+							<Toolbar />
+							{children}
+						</Box>
 					</Box>
-				</Box>
-			</body>
-		</html>
+				</body>
+			</html>
+		</>
 	);
 }
