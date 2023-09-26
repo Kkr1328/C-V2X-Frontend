60301bc7f1924b2ea0e5a514fd8815c585f07820
Chinnapat.N <kharkkr@gmail.com>
2023-09-26 10:37:51 +0700

    Add table in cars page

--- src/app/entity_management/cars/page.tsx
+++ src/app/entity_management/cars/page.tsx
@@ -5,13 +5,15 @@ import PageTitle from '@/components/common/PageTitle';
 import { Card, Divider, Stack } from '@mui/material';
 import Filter from '@/components/module/Filter';
 import ButtonCV2X from '@/components/common/ButtonCV2X';
-import {
-	CarActionModalTemplate,
-	CarFilterTemplate,
-} from '@/constants/TEMPLATE';
 import ModalCV2X from '@/components/common/ModalCV2X';
 import ModalInputs from '@/components/module/ModalInputs';
 import { useState } from 'react';
+import TableCV2X from '@/components/module/TableCV2X';
+import { CarsTableRowProps } from '@/types/ENTITY_TABLE';
+import { CarsTableTemplate } from '@/templates/ENTITY_TABLE';
+import { MockedCarsTableContent } from '@/mock/ENTITY_TABLE';
+import { CarFilterTemplate } from '@/templates/FILTER';
+import { CarActionModalTemplate } from '@/templates/ACTION_MODAL';
 
 export default function Home() {
 	const [openRegisterModal, setOpenRegisterModal] = useState<boolean>(false);
@@ -20,7 +22,7 @@ export default function Home() {
 		<>
 			<ModalCV2X
 				title={MODAL_LABEL.REGISTER_CAR}
-				variant="Register"
+				variant={BUTTON_LABEL.REGISTER}
 				open={openRegisterModal}
 				handleOnClose={() => setOpenRegisterModal(false)}
 			>
@@ -47,6 +49,10 @@ export default function Home() {
 								variant="outlined"
 							/>
 						</Stack>
+						<TableCV2X<CarsTableRowProps>
+							columns={CarsTableTemplate}
+							rows={MockedCarsTableContent}
+						/>
 					</Stack>
 				</Card>
 			</Stack>
