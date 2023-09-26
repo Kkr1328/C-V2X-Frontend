4275ae05a4197a0ae9310fb25852b46f181887b5
Chinnapat.N <kharkkr@gmail.com>
2023-09-26 10:18:21 +0700

    Add entity table mocked data

---
+++ src/mock/ENTITY_TABLE.tsx
@@ -0,0 +1,191 @@
+import {
+	CamerasTableRowProps,
+	CarsTableRowProps,
+	DriversTableRowProps,
+	RSUsTableRowProps,
+} from '@/types/ENTITY_TABLE';
+
+export const MockedCarsTableContent: CarsTableRowProps[] = [
+	{
+		id: 'carxxx01',
+		name: 'Car01',
+		license_plate: 'AB 0001',
+		model: 'Model01',
+		camera: 'temp',
+		driver: 'Driver01',
+	},
+	{
+		id: 'carxxx02',
+		name: 'Car02',
+		license_plate: 'AB 0002',
+		model: 'Model02',
+		camera: 'temp',
+		driver: 'Driver02',
+	},
+	{
+		id: 'carxxx03',
+		name: 'Car03',
+		license_plate: 'AB 0003',
+		model: 'Model03',
+		camera: 'temp',
+		driver: 'Driver03',
+	},
+	{
+		id: 'carxxx04',
+		name: 'Car04',
+		license_plate: 'AB 0004',
+		model: 'Model04',
+		camera: 'temp',
+		driver: 'Driver04',
+	},
+	{
+		id: 'carxxx05',
+		name: 'Car05',
+		license_plate: 'AB 0005',
+		model: 'Model05',
+		camera: 'temp',
+		driver: 'Driver05',
+	},
+	{
+		id: 'carxxx06',
+		name: 'Car06',
+		license_plate: 'AB 0006',
+		model: 'Model06',
+		camera: 'temp',
+		driver: 'Driver06',
+	},
+	{
+		id: 'carxxx07',
+		name: 'Car07',
+		license_plate: 'AB 0007',
+		model: 'Model07',
+		camera: 'temp',
+		driver: 'Driver07',
+	},
+];
+export const MockedDriversTableContent: DriversTableRowProps[] = [
+	{
+		id: 'driverxxx01',
+		name: 'Driver01',
+		username: 'Somchai01',
+		phone_no: '098xxxx001',
+	},
+	{
+		id: 'driverxxx02',
+		name: 'Driver02',
+		username: 'Somchai02',
+		phone_no: '098xxxx002',
+	},
+	{
+		id: 'driverxxx03',
+		name: 'Driver03',
+		username: 'Somchai03',
+		phone_no: '098xxxx003',
+	},
+	{
+		id: 'driverxxx04',
+		name: 'Driver04',
+		username: 'Somchai04',
+		phone_no: '098xxxx004',
+	},
+	{
+		id: 'driverxxx05',
+		name: 'Driver05',
+		username: 'Somchai05',
+		phone_no: '098xxxx005',
+	},
+	{
+		id: 'driverxxx06',
+		name: 'Driver06',
+		username: 'Somchai06',
+		phone_no: '098xxxx006',
+	},
+	{
+		id: 'driverxxx07',
+		name: 'Driver07',
+		username: 'Somchai07',
+		phone_no: '098xxxx007',
+	},
+];
+export const MockedCamerasTableContent: CamerasTableRowProps[] = [
+	{
+		id: 'rsuxxx01',
+		name: 'RSU01',
+		position: 'Front',
+		car: 'Car01',
+	},
+	{
+		id: 'rsuxxx02',
+		name: 'RSU02',
+		position: 'Back',
+		car: 'Car02',
+	},
+	{
+		id: 'rsuxxx03',
+		name: 'RSU03',
+		position: 'Front',
+		car: 'Car03',
+	},
+	{
+		id: 'rsuxxx04',
+		name: 'RSU04',
+		position: 'Back',
+		car: 'Car04',
+	},
+	{
+		id: 'rsuxxx05',
+		name: 'RSU05',
+		position: 'Front',
+		car: 'Car05',
+	},
+	{
+		id: 'rsuxxx06',
+		name: 'RSU06',
+		position: 'Back',
+		car: 'Car06',
+	},
+	{
+		id: 'rsuxxx07',
+		name: 'RSU07',
+		position: 'Front',
+		car: 'Car07',
+	},
+];
+
+export const MockedRSUsTableContent: RSUsTableRowProps[] = [
+	{
+		id: 'rsuxxx01',
+		name: 'RSU01',
+		rec_speed: 50,
+	},
+	{
+		id: 'rsuxxx02',
+		name: 'RSU02',
+		rec_speed: 70,
+	},
+	{
+		id: 'rsuxxx03',
+		name: 'RSU03',
+		rec_speed: 30,
+	},
+	{
+		id: 'rsuxxx04',
+		name: 'RSU04',
+		rec_speed: 90,
+	},
+	{
+		id: 'rsuxxx05',
+		name: 'RSU05',
+		rec_speed: 80,
+	},
+	{
+		id: 'rsuxxx06',
+		name: 'RSU06',
+		rec_speed: 20,
+	},
+	{
+		id: 'rsuxxx07',
+		name: 'RSU07',
+		rec_speed: 40,
+	},
+];
