b426355a567d69f415e3f51d86bab091e94b8fd0
Chinnapat.N <kharkkr@gmail.com>
2023-09-27 09:20:21 +0700

    Utilize LABEL to label

--- src/templates/ENTITY_TABLE.tsx
+++ src/templates/ENTITY_TABLE.tsx
@@ -1,144 +1,145 @@
+import { INPUT_LABEL } from '@/constants/LABEL';
 import {
 	TableHeaderProps,
-	CarsTableRowProps,
-	DriversTableRowProps,
-	CamerasTableRowProps,
-	RSUsTableRowProps,
+	CarsTableProps,
+	DriversTableProps,
+	CamerasTableProps,
+	RSUsTableProps,
 } from '@/types/ENTITY_TABLE';
 
-export const CarsTableTemplate: TableHeaderProps<CarsTableRowProps>[] = [
+export const CarsTableTemplate: TableHeaderProps<CarsTableProps>[] = [
 	{
 		id: 'id',
-		label: 'ID',
+		label: INPUT_LABEL.ID,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'name',
-		label: 'Name',
+		label: INPUT_LABEL.NAME,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'license_plate',
-		label: 'License Plate',
+		label: INPUT_LABEL.LICENSE_PLATE,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'model',
-		label: 'Model',
+		label: INPUT_LABEL.MODEL,
 		align: 'left',
 		isSorted: true,
 	},
 	{
-		id: 'camera',
-		label: 'Cameras',
+		id: 'cameras',
+		label: INPUT_LABEL.CAMERAS,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'driver',
-		label: 'Driver',
+		label: INPUT_LABEL.DRIVER,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'action',
-		label: 'Action',
+		label: INPUT_LABEL.ACTION,
 		align: 'center',
 		isSorted: false,
 	},
 ];
 
-export const DriversTableTemplate: TableHeaderProps<DriversTableRowProps>[] = [
+export const DriversTableTemplate: TableHeaderProps<DriversTableProps>[] = [
 	{
 		id: 'id',
-		label: 'ID',
+		label: INPUT_LABEL.ID,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'name',
-		label: 'Name',
+		label: INPUT_LABEL.NAME,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'username',
-		label: 'Username',
+		label: INPUT_LABEL.USERNAME,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'phone_no',
-		label: 'Phone NO.',
+		label: INPUT_LABEL.PHONE_NO,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'action',
-		label: 'Action',
+		label: INPUT_LABEL.ACTION,
 		align: 'center',
 		isSorted: false,
 	},
 ];
 
-export const CamerasTableTemplate: TableHeaderProps<CamerasTableRowProps>[] = [
+export const CamerasTableTemplate: TableHeaderProps<CamerasTableProps>[] = [
 	{
 		id: 'id',
-		label: 'ID',
+		label: INPUT_LABEL.ID,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'name',
-		label: 'Name',
+		label: INPUT_LABEL.NAME,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'position',
-		label: 'Position',
+		label: INPUT_LABEL.POSITION,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'car',
-		label: 'Car',
+		label: INPUT_LABEL.CAR,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'action',
-		label: 'Action',
+		label: INPUT_LABEL.ACTION,
 		align: 'center',
 		isSorted: false,
 	},
 ];
 
-export const RSUsTableTemplate: TableHeaderProps<RSUsTableRowProps>[] = [
+export const RSUsTableTemplate: TableHeaderProps<RSUsTableProps>[] = [
 	{
 		id: 'id',
-		label: 'ID',
+		label: INPUT_LABEL.ID,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'name',
-		label: 'Name',
+		label: INPUT_LABEL.NAME,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'rec_speed',
-		label: 'Recommended speed (km/h)',
+		label: INPUT_LABEL.RECOMENDED_SPEED,
 		align: 'left',
 		isSorted: true,
 	},
 	{
 		id: 'action',
-		label: 'Action',
+		label: INPUT_LABEL.ACTION,
 		align: 'center',
 		isSorted: false,
 	},
