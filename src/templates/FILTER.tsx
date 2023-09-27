bd2da2cae90b82f67aef6278acc0e35c61be8124
Chinnapat.N <kharkkr@gmail.com>
2023-09-27 09:22:07 +0700

    Add id to filter template

--- src/templates/FILTER.tsx
+++ src/templates/FILTER.tsx
@@ -1,44 +1,57 @@
 import { INPUT_LABEL } from '@/constants/LABEL';
 import { InputFieldProp } from '@/types/COMMON';
+import {
+	CamerasTableProps,
+	CarsTableProps,
+	DriversTableProps,
+	RSUsTableProps,
+} from '@/types/ENTITY_TABLE';
 
-export const CarFilterTemplate: InputFieldProp[] = [
+export const CarFilterTemplate: InputFieldProp<CarsTableProps>[] = [
 	{
+		id: 'id',
 		label: INPUT_LABEL.ID,
 		type: 'TextField',
 		placeholder: 'ex. ID01',
 		row: 1,
 	},
 	{
+		id: 'name',
 		label: INPUT_LABEL.NAME,
 		type: 'TextField',
 		placeholder: 'ex. Car01',
 		row: 1,
 	},
 	{
+		id: 'license_plate',
 		label: INPUT_LABEL.LICENSE_PLATE,
 		type: 'TextField',
 		placeholder: 'ex. กข 1234',
 		row: 1,
 	},
 	{
+		id: 'model',
 		label: INPUT_LABEL.MODEL,
 		type: 'TextField',
 		placeholder: 'ex. ABC Model',
 		row: 1,
 	},
 	{
+		id: 'front_camera',
 		label: INPUT_LABEL.FRONT_CAMERA,
 		type: 'Select',
 		placeholder: 'ex. Camera01',
 		row: 2,
 	},
 	{
+		id: 'back_camera',
 		label: INPUT_LABEL.BACK_CAMERA,
 		type: 'Select',
 		placeholder: 'ex. Camera01',
 		row: 2,
 	},
 	{
+		id: 'driver',
 		label: INPUT_LABEL.DRIVER,
 		type: 'Select',
 		placeholder: 'ex. สมชาย สวัสดี',
@@ -46,26 +59,30 @@ export const CarFilterTemplate: InputFieldProp[] = [
 	},
 ];
 
-export const DriverFilterTemplate: InputFieldProp[] = [
+export const DriverFilterTemplate: InputFieldProp<DriversTableProps>[] = [
 	{
+		id: 'id',
 		label: INPUT_LABEL.ID,
 		type: 'TextField',
 		placeholder: 'ex. ID01',
 		row: 1,
 	},
 	{
+		id: 'name',
 		label: INPUT_LABEL.NAME,
 		type: 'TextField',
 		placeholder: 'ex. สมชาย สวัสดี',
 		row: 1,
 	},
 	{
+		id: 'username',
 		label: INPUT_LABEL.USERNAME,
 		type: 'TextField',
 		placeholder: 'ex. somchai',
 		row: 1,
 	},
 	{
+		id: 'phone_no',
 		label: INPUT_LABEL.PHONE_NO,
 		type: 'TextField',
 		placeholder: 'ex. 0993336666',
@@ -73,26 +90,30 @@ export const DriverFilterTemplate: InputFieldProp[] = [
 	},
 ];
 
-export const CameraFilterTemplate: InputFieldProp[] = [
+export const CameraFilterTemplate: InputFieldProp<CamerasTableProps>[] = [
 	{
+		id: 'id',
 		label: INPUT_LABEL.ID,
 		type: 'TextField',
 		placeholder: 'ex. ID01',
 		row: 1,
 	},
 	{
+		id: 'name',
 		label: INPUT_LABEL.NAME,
 		type: 'TextField',
 		placeholder: 'ex. Camera01',
 		row: 1,
 	},
 	{
+		id: 'position',
 		label: INPUT_LABEL.POSITION,
 		type: 'Select',
 		placeholder: 'ex. Front',
 		row: 1,
 	},
 	{
+		id: 'car',
 		label: INPUT_LABEL.CAR,
 		type: 'Select',
 		placeholder: 'ex. Car01',
@@ -100,22 +121,25 @@ export const CameraFilterTemplate: InputFieldProp[] = [
 	},
 ];
 
-export const RSUFilterTemplate: InputFieldProp[] = [
+export const RSUFilterTemplate: InputFieldProp<RSUsTableProps>[] = [
 	{
+		id: 'id',
 		label: INPUT_LABEL.ID,
 		type: 'TextField',
 		placeholder: 'ex. ID01',
 		row: 1,
 	},
 	{
+		id: 'name',
 		label: INPUT_LABEL.NAME,
 		type: 'TextField',
 		placeholder: 'ex. RSU01',
 		row: 1,
 	},
 	{
+		id: 'rec_speed',
 		label: INPUT_LABEL.RECOMENDED_SPEED,
-		type: 'Select',
+		type: 'TextField',
 		placeholder: '50',
 		row: 1,
 	},
