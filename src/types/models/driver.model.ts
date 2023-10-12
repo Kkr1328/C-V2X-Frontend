9af062cf8b7cea0f14c62664914265f02bc8deb9
Chinnapat.N <kharkkr@gmail.com>
2023-10-12 16:35:55 +0700

    Implement driver model type

---
+++ src/types/models/driver.model.ts
@@ -0,0 +1,33 @@
+import { IQuerry } from '../common/query.model';
+
+export interface IGetDriverQuery extends IQuerry {}
+export interface IUpdateDriverQuery {
+	query: IQuerry;
+	request: IDriverRequest;
+}
+export interface IDeleteDriverQuery extends IQuerry {}
+
+export interface IDriverRequest {
+	first_name: string;
+	last_name: string;
+	phone_no: string;
+	username: string;
+	password: string;
+}
+
+export interface IGetDriversRequest {
+	id?: string;
+	name?: string;
+	username?: string;
+	phone_no?: string;
+}
+export interface ICreateDriverRequest extends IDriverRequest {}
+export interface IUpdateDriverRequest extends IDriverRequest {}
+
+export interface IDriver {
+	_id: string;
+	first_name: string;
+	last_name: string;
+	phone_no: string;
+	username: string;
+}
