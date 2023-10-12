f3da7f914bf5a68bd03a23b9d0b1e7901933b301
Chinnapat.N <kharkkr@gmail.com>
2023-10-12 16:30:41 +0700

    Implement create driver redux gateway

---
+++ src/redux/create-driver/create-driver-reducer.ts
@@ -0,0 +1,23 @@
+import { createReducer } from '@reduxjs/toolkit';
+import { initialState } from './create-driver-state';
+import { FETCH_CREATE_DRIVER } from './create-driver-action';
+import { ErrorCommon } from '@/types/common/error.model';
+
+const createDriverReducer = createReducer(initialState, (builder) =>
+	builder
+		.addCase(FETCH_CREATE_DRIVER.pending, (state) => ({
+			...state,
+			loading: true,
+		}))
+		.addCase(FETCH_CREATE_DRIVER.fulfilled, (state) => ({
+			...state,
+			loading: false,
+		}))
+		.addCase(FETCH_CREATE_DRIVER.rejected, (state, action) => ({
+			...state,
+			loading: false,
+			error: action.payload as ErrorCommon,
+		}))
+);
+
+export default createDriverReducer;
