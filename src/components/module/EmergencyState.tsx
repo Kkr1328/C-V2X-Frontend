cde2ff6c3427dcdee38ddae9e35cee868d5771ee
Chinnapat.N <kharkkr@gmail.com>
2023-10-18 00:47:24 +0700

    Add beautiful-dnd to emergency state

--- src/components/module/EmergencyState.tsx
+++ src/components/module/EmergencyState.tsx
@@ -2,8 +2,10 @@ import { Card, Stack } from '@mui/material';
 import NoData from '../common/NoData';
 import EmergencyCard from '../common/EmergencyCard';
 import { Emergency } from '@/types/COMMON';
+import { Droppable } from 'react-beautiful-dnd';
 
 interface EmergencyStateProps {
+	droppableId: string;
 	title: Emergency;
 	children?: React.ReactNode;
 	isLoading?: boolean;
@@ -11,39 +13,38 @@ interface EmergencyStateProps {
 
 export default function EmergencyState(props: EmergencyStateProps) {
 	return (
-		<Card className="h-[calc(100vh-192px)] bg-white rounded-lg p-32 w-fit">
-			<Stack className="gap-16 items-center h-full">
-				<Stack direction="row" className="w-full">
-					<p className="inline-block align-baseline font-istok text-dark_text_grey text-h3">
-						{props.title}
-					</p>
-					<div className="grow" />
-					<p className="inline-block align-baseline font-istok text-light_text_grey text-h3">
-						7
-					</p>
-				</Stack>
-				{props.isLoading ? (
-					<Stack className="gap-16 items-center h-full overflow-y-auto">
-						{/* <EmergencyCard isLoading={true} />
-						<EmergencyCard isLoading={true} />
-						<EmergencyCard isLoading={true} />
-						<EmergencyCard isLoading={true} />
-						<EmergencyCard isLoading={true} /> */}
-						<EmergencyCard carName="hihi" driverPhoneNo="ksldfjklsdf" />
-						<EmergencyCard carName="hihi" driverPhoneNo="ksldfjklsdf" />
-						<EmergencyCard carName="hihi" driverPhoneNo="ksldfjklsdf" />
-						<EmergencyCard carName="hihi" driverPhoneNo="ksldfjklsdf" />
-						<EmergencyCard carName="hihi" driverPhoneNo="ksldfjklsdf" />
-						<EmergencyCard carName="hihi" driverPhoneNo="ksldfjklsdf" />
-					</Stack>
-				) : props.children ? (
-					props.children
-				) : (
-					<div className="w-300 h-full flex items-center">
-						<NoData />
-					</div>
-				)}
-			</Stack>
-		</Card>
+		<Droppable key={props.title} droppableId={props.droppableId}>
+			{(provided) => {
+				return (
+					<Card
+						ref={provided.innerRef}
+						{...provided.droppableProps}
+						className="h-[calc(100vh-192px)] bg-white rounded-lg p-32 w-fit"
+					>
+						<Stack className="items-center h-full">
+							<Stack direction="row" className="w-full">
+								<p className="inline-block align-baseline font-istok text-dark_text_grey text-h3">
+									{props.title}
+								</p>
+								<div className="grow" />
+								<p className="inline-block align-baseline font-istok text-light_text_grey text-h3">
+									7
+								</p>
+							</Stack>
+							{props.isLoading ? (
+								<Stack className="items-center h-full overflow-y-auto"></Stack>
+							) : props.children ? (
+								props.children
+							) : (
+								<div className="w-300 h-full flex items-center">
+									<NoData />
+								</div>
+							)}
+						</Stack>
+						{provided.placeholder}
+					</Card>
+				);
+			}}
+		</Droppable>
 	);
 }
