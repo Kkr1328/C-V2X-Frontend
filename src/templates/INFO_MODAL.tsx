9de636dcedf373a25ae0bbc329cc380f40402aa4
Chinnapat.N <kharkkr@gmail.com>
2023-12-24 21:39:58 +0700

    [UPDATE] Add modal responsive template

--- src/templates/INFO_MODAL.tsx
+++ src/templates/INFO_MODAL.tsx
@@ -1,90 +1,157 @@
+// react
+import { useEffect, useState } from 'react';
+// const
 import { INPUT_LABEL } from '@/constants/LABEL';
+// types
 import { InputFieldProp } from '@/types/COMMON';
 import { ICamera } from '@/types/models/camera.model';
-import { ICar, ICarInfo } from '@/types/models/car.model';
+import { ICarInfo } from '@/types/models/car.model';
 import { IDriver } from '@/types/models/driver.model';
 import { IRSU } from '@/types/models/rsu.model';
 
-export const CarInfoModalTemplate: InputFieldProp<ICarInfo>[] = [
-	{
-		id: 'license_plate',
-		label: INPUT_LABEL.LICENSE_PLATE,
-		type: 'TextField',
-		row: 1,
-	},
-	{
-		id: 'model',
-		label: INPUT_LABEL.MODEL,
-		type: 'TextField',
-		row: 1,
-	},
-	{
-		id: 'driver',
-		label: INPUT_LABEL.DRIVER,
-		type: 'TextField',
-		row: 2,
-	},
-	{
-		id: 'front_cam_name',
-		label: INPUT_LABEL.CAMERAS,
-		type: 'TextField',
-		row: 3,
-	},
-	{
-		id: 'front_cam_position',
-		label: INPUT_LABEL.POSITION,
-		type: 'TextField',
-		row: 3,
-	},
-	{
-		id: 'back_cam_name',
-		label: INPUT_LABEL.CAMERAS,
-		type: 'TextField',
-		row: 4,
-	},
-	{
-		id: 'back_cam_position',
-		label: INPUT_LABEL.POSITION,
-		type: 'TextField',
-		row: 4,
-	},
-];
+export function CarInfoModalTemplate(): InputFieldProp<ICarInfo>[] {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
 
-export const DriverInfoModalTemplate: InputFieldProp<IDriver>[] = [
-	{
-		id: 'username',
-		label: INPUT_LABEL.USERNAME,
-		type: 'TextField',
-		row: 1,
-	},
-	{
-		id: 'phone_no',
-		label: INPUT_LABEL.PHONE_NO,
-		type: 'TextField',
-		row: 1,
-	},
-];
+		window.addEventListener('resize', handleResize);
 
-export const CameraInfoModalTemplate: InputFieldProp<ICamera>[] = [
-	{
-		id: 'car',
-		label: INPUT_LABEL.CAR,
-		type: 'Select',
-		row: 1,
-	},
-	{
-		id: 'position',
-		label: INPUT_LABEL.POSITION,
-		type: 'Select',
-		row: 1,
-	},
-];
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+	const isUseCompact = windowWidth <= 640;
 
-export const RSUInfoModalTemplate: InputFieldProp<IRSU>[] = [
-	{
-		id: 'recommended_speed',
-		label: INPUT_LABEL.RECOMENDED_SPEED,
-		type: 'TextField',
-		row: 1,
-	},
-];
+	return [
+		{
+			id: 'license_plate',
+			label: INPUT_LABEL.LICENSE_PLATE,
+			type: 'TextField',
+			row: isUseCompact ? 1 : 1,
+		},
+		{
+			id: 'model',
+			label: INPUT_LABEL.MODEL,
+			type: 'TextField',
+			row: isUseCompact ? 2 : 1,
+		},
+		{
+			id: 'driver',
+			label: INPUT_LABEL.DRIVER,
+			type: 'TextField',
+			row: isUseCompact ? 3 : 2,
+		},
+		{
+			id: 'front_cam_name',
+			label: INPUT_LABEL.CAMERAS,
+			type: 'TextField',
+			row: isUseCompact ? 4 : 3,
+		},
+		{
+			id: 'front_cam_position',
+			label: INPUT_LABEL.POSITION,
+			type: 'TextField',
+			row: isUseCompact ? 5 : 3,
+		},
+		{
+			id: 'back_cam_name',
+			label: INPUT_LABEL.CAMERAS,
+			type: 'TextField',
+			row: isUseCompact ? 6 : 4,
+		},
+		{
+			id: 'back_cam_position',
+			label: INPUT_LABEL.POSITION,
+			type: 'TextField',
+			row: isUseCompact ? 7 : 4,
+		},
+	];
+}
+
+export function DriverInfoModalTemplate(): InputFieldProp<IDriver>[] {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+	const isUseCompact = windowWidth <= 640;
+
+	return [
+		{
+			id: 'username',
+			label: INPUT_LABEL.USERNAME,
+			type: 'TextField',
+			row: isUseCompact ? 1 : 1,
+		},
+		{
+			id: 'phone_no',
+			label: INPUT_LABEL.PHONE_NO,
+			type: 'TextField',
+			row: isUseCompact ? 2 : 1,
+		},
+	];
+}
+
+export function CameraInfoModalTemplate(): InputFieldProp<ICamera>[] {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+	const isUseCompact = windowWidth <= 640;
+	return [
+		{
+			id: 'car',
+			label: INPUT_LABEL.CAR,
+			type: 'Select',
+			row: isUseCompact ? 1 : 1,
+		},
+		{
+			id: 'position',
+			label: INPUT_LABEL.POSITION,
+			type: 'Select',
+			row: isUseCompact ? 2 : 1,
+		},
+	];
+}
+
+export function RSUInfoModalTemplate(): InputFieldProp<IRSU>[] {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth);
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+	const isUseCompact = windowWidth <= 640;
+
+	return [
+		{
+			id: 'recommended_speed',
+			label: INPUT_LABEL.RECOMENDED_SPEED,
+			type: 'TextField',
+			row: isUseCompact ? 1 : 1,
+		},
+	];
+}
