1d7f36e3243225895046b3f701fe55184b52ae52
Chinnapat.N <kharkkr@gmail.com>
2024-01-02 11:19:15 +0700

    [UPDATE] Minor change

--- src/components/common/NoData.tsx
+++ src/components/common/NoData.tsx
@@ -1,6 +1,5 @@
 import { BUTTON_LABEL } from '@/constants/LABEL';
 import IconMapper from '@/utils/IconMapper';
-import { Stack } from '@mui/material';
 
 interface NoDataProps {
 	size?: 'small' | 'medium' | 'large';
@@ -8,32 +7,30 @@ interface NoDataProps {
 
 export default function NoData(props: NoDataProps) {
 	return (
-		<div className="flex w-full h-full items-center justify-center">
-			<Stack className="gap-4 text-black">
-				<div className="self-center">
-					<IconMapper
-						icon={BUTTON_LABEL.NO_DATA}
-						size={
-							props.size === 'medium'
-								? '36px'
-								: props.size === 'large'
-								? '48px'
-								: '24px'
-						}
-					/>
-				</div>
-				<p
-					className={`self-center inline-block align-baseline font-istok text-black ${
+		<div className="flex flex-col w-full h-full gap-4 text-black items-center justify-center">
+			<div className="self-center">
+				<IconMapper
+					icon={BUTTON_LABEL.NO_DATA}
+					size={
 						props.size === 'medium'
-							? 'text-h4'
+							? '36px'
 							: props.size === 'large'
-							? 'text-h3'
-							: 'text-h5'
-					}`}
-				>
-					{BUTTON_LABEL.NO_DATA}
-				</p>
-			</Stack>
+							? '48px'
+							: '24px'
+					}
+				/>
+			</div>
+			<p
+				className={`self-center inline-block align-baseline font-istok text-black ${
+					props.size === 'medium'
+						? 'text-h4'
+						: props.size === 'large'
+						? 'text-h3'
+						: 'text-h5'
+				}`}
+			>
+				{BUTTON_LABEL.NO_DATA}
+			</p>
 		</div>
 	);
 }
