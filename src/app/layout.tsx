edf867830e7cf9954632891dcd43390f2b95bdf2
Chinnapat.N <kharkkr@gmail.com>
2024-01-20 14:06:24 +0700

    [UPDATE] Add speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Istok_Web } from 'next/font/google';
 import LayoutWrapper from '@/components/module/LayoutWrapper';
 import { Suspense } from 'react';
 import Loading from './loading';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const istok_web = Istok_Web({
 	weight: ['400', '700'],
@@ -27,7 +28,10 @@ export default function RootLayout({
 			<html lang="en" className={`${istok_web.variable} font-sans`}>
 				<body id="__next" suppressHydrationWarning={true}>
 					<LayoutWrapper>
-						<Suspense fallback={<Loading />}>{children}</Suspense>
+						<Suspense fallback={<Loading />}>
+							{children}
+							<SpeedInsights />
+						</Suspense>
 					</LayoutWrapper>
 				</body>
 			</html>
